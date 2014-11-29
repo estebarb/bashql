121e98e0b35ec06cee1310cb94acc2ba57090eea
Esteban RB <estebarb@gmail.com>
2014-11-29 15:22:12 -0600

    Se completó el comando bqlfrom

---
+++ bqlfrom/from.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"os"
+	"flag"
+	"bufio"
+	"encoding/csv"
+	"unicode/utf8"
+)
+
+var separator = flag.String("d", ",", "field delimiter (',' by default)")
+var comment = flag.String("c", "", "help message for flagname")
+
+func main(){
+	flag.Parse()
+	
+	// Abre el archivo para ser leído
+	file, err := os.Open(flag.Arg(0))	
+	if err != nil{
+		panic (err)
+	}
+	
+	reader := bufio.NewReader(file)
+	
+	// Configura la lectura del CSV
+	r := csv.NewReader(reader)
+	r.Comma,_ = utf8.DecodeRuneInString(*separator)
+	if *comment != ""{
+		r.Comment,_ = utf8.DecodeRuneInString(*comment)
+	}
+		
+	// Abrimos el nuevo CSV en la salida estandar
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	w := csv.NewWriter(writer)
+	defer w.Flush()
+	
+	// Imprime el contenido del archivo	
+	col, er := r.Read()
+	for er == nil {
+		w.Write(col)		
+		col, er = r.Read()		
+	}
+}
